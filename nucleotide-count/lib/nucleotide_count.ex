401bdea03ebc185b6feea62b5e4af633dcef5dae
kathryn-sanders <kathryn.sanders.ks@gmail.com>
2020-01-05 21:03:17 -0500

    Completed Nucleotide Count Exercise

--- nucleotide-count/lib/nucleotide_count.ex
+++ nucleotide-count/lib/nucleotide_count.ex
@@ -1,5 +1,5 @@
 defmodule NucleotideCount do
-  @nucleotides [?A, ?C, ?G, ?T]
+  # @nucleotides [?A, ?C, ?G, ?T]
 
   @doc """
   Counts individual nucleotides in a DNA strand.
@@ -14,6 +14,13 @@ defmodule NucleotideCount do
   """
   @spec count(charlist(), char()) :: non_neg_integer()
   def count(strand, nucleotide) do
+    Enum.reduce(strand, 0, fn current, n_count ->
+      if current == nucleotide do
+        n_count + 1
+      else
+        n_count
+      end
+    end)
   end
 
   @doc """
@@ -26,5 +33,14 @@ defmodule NucleotideCount do
   """
   @spec histogram(charlist()) :: map()
   def histogram(strand) do
+    Enum.reduce(strand, %{?A => 0, ?T => 0, ?C => 0, ?G => 0}, fn current, nucleotides_count ->
+      case current do
+        ?A -> %{nucleotides_count | ?A => nucleotides_count[?A] + 1}
+        ?T -> %{nucleotides_count | ?T => nucleotides_count[?T] + 1}
+        ?C -> %{nucleotides_count | ?C => nucleotides_count[?C] + 1}
+        ?G -> %{nucleotides_count | ?G => nucleotides_count[?G] + 1}
+        _ -> nucleotides_count
+      end
+    end)
   end
 end
